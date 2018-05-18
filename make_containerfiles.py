825495024d244ea233cb9aa4f5f7cad245554d09
Carl Simon Adorf <csadorf@umich.edu>
2018-05-18 13:22:23 -0400

    Streamline the container file make script.

---
+++ make_containerfiles.py
@@ -0,0 +1,33 @@
+import jinja2
+
+
+CONTAINER_FILENAME = {
+    'singularity': 'Singularity',
+    'docker': 'Dockerfile',
+}
+
+
+def main():
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
+
+    for container_system in sorted(CONTAINER_FILENAME):
+        template = env.get_template(container_system + '.jinja')
+        for cuda in (False, True):
+            for system in ('flux', 'comet', 'bridges'):
+
+                image = 'glotzerlab/software'
+                fn = CONTAINER_FILENAME[container_system]
+
+                tag = 'cuda8-' + system if cuda else system
+                if tag:
+                    image += ':' + tag
+                    fn += '.' + tag
+
+                recipe = template.render({'base_image': image})
+                print("Writing '{}'.".format(fn))
+                with open(fn, 'w') as containerfile:
+                    containerfile.write(recipe + '\n')
+
+
+if __name__ == '__main__':
+    main()
